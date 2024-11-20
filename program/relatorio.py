afe69a3da3712274e45d4517ed57822a532aa6ba
hellolima <gabrielladelimadev@gmail.com>
2024-11-19 22:08:36 -0300

    fix: texto relatorio

--- program/relatorio.py
+++ program/relatorio.py
@@ -17,7 +17,7 @@ def gerarRelatorioCompressao(instancia):
         f"Tamanho do Texto Codificado (em bytes): {instancia.totalCodificadosBits / 8:.2f}",
         f"Taxa de Compressão Final: {taxaCompressaoFinal:.2f}%",
         f"Quantidade de códigos no dicionário: {instancia.dicionario.getTamanho()}",
-        f"Quantidade de memória utilizada no dicionário (Em bytes e em relação aos códigos): {instancia.totalEspacoDicionario/8}",
+        f"Quantidade de memória utilizada no dicionário (em bytes): {instancia.totalEspacoDicionario/8}",
         f"Tempo de Execução da Codificação: {instancia.tempoExecucaoCodificar:.4f} segundos",
         "======================================================================="
     ]
@@ -52,7 +52,7 @@ def gerarRelatorioDescompressao(instancia):
         f"Tamanho do Texto Descodificado (em bytes): {instancia.totalCodificadosBits / 8:.2f}",
         f"Taxa de Descompressão Final: {taxaDescompressaoFinal:.2f}%",
         f"Quantidade de códigos no dicionário: {instancia.dicionario.getTamanho()}",
-        f"Quantidade de memória utilizada no dicionário (Em bytes e em relação aos códigos): {instancia.totalEspacoDicionario/8}",
+        f"Quantidade de memória utilizada no dicionário (em bytes): {instancia.totalEspacoDicionario/8}",
         f"Tempo de Execução da Descodificação: {instancia.tempoExecucaoDecodificar:.4f} segundos",
         "======================================================================="
     ]
