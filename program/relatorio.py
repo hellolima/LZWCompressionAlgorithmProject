840eaf8f4bbe497f955d79d0301ea5c67dc6b0c6
hellolima <gabrielladelimadev@gmail.com>
2024-11-19 22:37:41 -0300

    fix

--- program/relatorio.py
+++ program/relatorio.py
@@ -3,14 +3,11 @@ import matplotlib.pyplot as plt
 from lzwTamanhoFixo import *
 
 def gerarRelatorioCompressao(instancia):
-    # Calcular a taxa de compressão final
     taxaCompressaoFinal = (1 - ((instancia.totalCodificadosBits / 8) / instancia.totalOriginalBytes)) * 100
     
-    # Cria o diretório "estatisticas" caso ele não exista
     if not os.path.exists('estatisticas'):
         os.makedirs('estatisticas')
     
-    # Relatório de compressão
     relatorio = [
         "===================== Relatório de Compressão LZW =====================",
         f"Tamanho do Texto Original (em bytes): {instancia.totalOriginalBytes:.2f}",
@@ -22,11 +19,9 @@ def gerarRelatorioCompressao(instancia):
         "======================================================================="
     ]
     
-    # Salvar o relatório na pasta 'estatisticas'
     with open('estatisticas/relatorio.txt', 'w') as arquivo:
         arquivo.write("\n".join(relatorio))
     
-    # Criar o gráfico da taxa de compressão
     plt.figure(figsize=(10, 6))
     plt.plot(instancia.tiposCodificacao, instancia.taxaCompressao, label='Taxa de Compressão', color='g')
     plt.xlabel('Passo da Codificação')
@@ -34,18 +29,14 @@ def gerarRelatorioCompressao(instancia):
     plt.title('Taxa de Compressão ao Longo do Processo')
     plt.legend()
     
-    # Salvar o gráfico na pasta 'estatisticas'
     plt.savefig('estatisticas/taxaCompressaoAoLongoProcessamento.png', format='png')
 
 def gerarRelatorioDescompressao(instancia):
-    # Calcular a taxa de descompressão final
     taxaDescompressaoFinal = (1 - ((instancia.totalCodificadosBits / 8) / instancia.totalOriginalBytes)) * 100
     
-    # Cria o diretório "estatisticas" caso ele não exista
     if not os.path.exists('estatisticas'):
         os.makedirs('estatisticas')
     
-    # Relatório de descompressão
     relatorio = [
         "===================== Relatório de Descompressão LZW =====================",
         f"Tamanho do Texto Original (em bytes): {instancia.totalOriginalBytes:.2f}",
@@ -57,11 +48,9 @@ def gerarRelatorioDescompressao(instancia):
         "======================================================================="
     ]
     
-    # Salvar o relatório na pasta 'estatisticas'
-    with open('estatisticas/relatorio_descompressao.txt', 'w') as arquivo:
+    with open('estatisticas/relatorioDescompressao.txt', 'w') as arquivo:
         arquivo.write("\n".join(relatorio))
-    
-    # Criar o gráfico da taxa de descompressão
+
     plt.figure(figsize=(10, 6))
     plt.plot(instancia.tiposDescompressao, instancia.taxaDescompressao, label='Taxa de Descompressão', color='b')
     plt.xlabel('Passo da Descompressão')
@@ -69,5 +58,4 @@ def gerarRelatorioDescompressao(instancia):
     plt.title('Taxa de Descompressão ao Longo do Processo')
     plt.legend()
     
-    # Salvar o gráfico na pasta 'estatisticas'
     plt.savefig('estatisticas/taxaDescompressaoAoLongoProcessamento.png', format='png')
